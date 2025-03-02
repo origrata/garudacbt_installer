c8b9bd85b9e80f27c26fc8363a704cd46ae11d1b
Ori Grata <42106367+origrata@users.noreply.github.com>
2025-03-02 10:00:22 +0700

    Update install.sh
    Membuat directory ssl dan genereta ssl

--- install.sh
+++ install.sh
@@ -187,6 +187,10 @@ EOL
 #Tempat simpan data mariadb docker
 mkdir -p mariadb_data
 
+#Aktifkan SSL
+mkdir ssl 
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ssl/nginx.key -out ssl/nginx.crt
+
 echo "Proses instalasi garudacbt sedang berlangsung..."
 docker-compose up -d --build
 
